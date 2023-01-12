fc5d3a8e2ee38f1efdc98ca57226648a86840fee
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-01-12 13:28:30 +0530

    aadhar template

---
+++ src/FolderProcess.py
@@ -0,0 +1,21 @@
+import os
+
+class ExtractAndForward():
+    def __init__(self, name):
+        self.name = name
+
+    def ExtractAadhar(self):
+        folder_path = '/path/to/folder'
+
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as f:
+                    content = f.read()
+                    # Do something with the file's content
+                    print(content)
+            
+    
+
+
+
