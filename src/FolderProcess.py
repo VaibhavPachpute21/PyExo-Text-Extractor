224f147d23da07d14c8fa94a893f5399b8942e87
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-01-12 19:52:22 +0530

    image matching

--- src/FolderProcess.py
+++ src/FolderProcess.py
@@ -7,31 +7,21 @@ class ExtractAndForward():
         pass    
 
 
-    def ImageSplitter(self,i,j):
+    def ImageSplitter(self,filePath,templatePath):
+        img = cv2.imread(filePath)
+        template = cv2.imread(templatePath)
+        # bnwImg1=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-        img = cv2.imread(i)
-        template = cv2.imread(j)
-
-        bnwImg1=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        
-
-        bnwImg2=cv2.cvtColor(template,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('GRAY Template IMG',bnwImg2)
+        # bnwImg2=cv2.cvtColor(template,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('GRAY Template IMG',template)
         cv2.waitKey(0)
 
         """ Match """
 
         img_copy = img.copy()
-
-
         result = cv2.matchTemplate(img_copy, template, cv2.TM_CCOEFF_NORMED)
-
-     
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-
-        
         w, h = template.shape[1], template.shape[0]
-
         
         cropped = img_copy[max_loc[1]:max_loc[1]+h, max_loc[0]:max_loc[0]+w]
         cv2.imshow('GRAY Template IMG',cropped)
@@ -42,13 +32,10 @@ class ExtractAndForward():
         filesPaths = self.FindPaths()
         print(filesPaths)
         if len(filesPaths) > 0:
-            templates = ['src/imgs/templates/aadharFront.jpg','src/imgs/templates/driving.jpg','src/imgs/templates/pan.jpg','src/imgs/templates/passport.png','src/imgs/templates/voter-id.jpg']
-            for i in filesPaths:
-                for j in templates:
-                    self.ImageSplitter(i,j)
-                
-            
-
+            templates = ['src/imgs/templates/aadharFront.jpg','src/imgs/templates/driving.jpg','src/imgs/templates/pan-card.png','src/imgs/templates/passport.png','src/imgs/templates/voter.jpg']
+            for filePath in filesPaths:
+                for templatePath in templates:
+                    self.ImageSplitter(filePath,templatePath)
 
     def FindPaths(self):
         folder_path = 'src/test'
@@ -61,9 +48,7 @@ class ExtractAndForward():
         
         return arr
 
-                
-            
-    
+
 files = ExtractAndForward()
 files.ExtractAadhar()
 
