43d0c4edd637719e805cbcb28065a4827a81b758
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-01-12 18:36:13 +0530

    Shaping

--- src/FolderProcess.py
+++ src/FolderProcess.py
@@ -8,14 +8,41 @@ class ExtractAndForward():
 
 
     def ImageSplitter(self,i,j):
-        print(i,j)
 
+        img = cv2.imread(i)
+        template = cv2.imread(j)
+
+        bnwImg1=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        
+
+        bnwImg2=cv2.cvtColor(template,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('GRAY Template IMG',bnwImg2)
+        cv2.waitKey(0)
+
+        """ Match """
+
+        img_copy = img.copy()
+
+
+        result = cv2.matchTemplate(img_copy, template, cv2.TM_CCOEFF_NORMED)
+
+     
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+        
+        w, h = template.shape[1], template.shape[0]
+
+        
+        cropped = img_copy[max_loc[1]:max_loc[1]+h, max_loc[0]:max_loc[0]+w]
+        cv2.imshow('GRAY Template IMG',cropped)
+        cv2.waitKey(0)
+               
 
     def ExtractAadhar(self):
         filesPaths = self.FindPaths()
         print(filesPaths)
         if len(filesPaths) > 0:
-            templates = ['src/imgs/templates/aadharFront.jpg','src/imgs/templates/driving.jpg','src/imgs/templates/pan.jpg','src/imgs/templates/passport.jpg']
+            templates = ['src/imgs/templates/aadharFront.jpg','src/imgs/templates/driving.jpg','src/imgs/templates/pan.jpg','src/imgs/templates/passport.png','src/imgs/templates/voter-id.jpg']
             for i in filesPaths:
                 for j in templates:
                     self.ImageSplitter(i,j)
