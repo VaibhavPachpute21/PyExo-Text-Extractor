4c0ead42e19affb8a0933f98e14cb99f6d717f4c
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-01-13 15:16:52 +0530

    text matching

--- src/Aadhar_textDetect.py
+++ src/Aadhar_textDetect.py
@@ -1,15 +1,27 @@
 import pytesseract
 import cv2
+import os
+import re
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+os.environ['TESSDATA_PREFIX'] = 'C:\Program Files\Tesseract-OCR\\tessdata'
 class ExtractData():
    def __init__(self,file):
       self.file=file
-      print("Extract Data",file)
-      image=cv2.imread('src/test/adharcard.jpg');
-      text = pytesseract.image_to_string(image,lang='eng + hin')
-      print(text)
+
+      image=cv2.imread(file);
+
+      text = pytesseract.image_to_string(image,lang='eng+hin')
+
+      string = text
+
+      # Check if the string contains 12 digits using regular expression
+      match = re.search(r'\d{12}', string)
+
+      if match:
+         print(text)
+      else:
+         print("The string does not contain 12 digits.")
 
 
       pass
--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import cv2
 from PIL import Image
-from pdf2image import convert_from_path
+import os
 
-documentsIMG=convert_from_path('D:\Web\eCell\src\\test\documents.pdf')
 
-for document in range(len(documentsIMG)):
-    documentsIMG[document].save('doc'+document+'.jpg','JPEG')
+
+
+# from pdf2image import convert_from_path
+# documentsIMG=convert_from_path('D:\Web\eCell\src\\test\documents.pdf')
+# for document in range(len(documentsIMG)):
+#     documentsIMG[document].save('doc'+document+'.jpg','JPEG')
     # cv2.imshow('Document',documentsIMG[document])
     # cv2.waitKey(0)
-print(self.MaxMatches.index(max(self.InterMediateMax)))
+# print(self.MaxMatches.index(max(self.InterMediateMax)))
 # bnwImg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY);
 # cv2.imshow('GRAY IMG',bnwImg);
 # cv2.waitKey(0)
