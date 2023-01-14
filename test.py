2e5bcb7f0cff783b9e39c7c8e2a935509226c255
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-01-14 14:39:43 +0530

    Khaana

--- test.py
+++ test.py
@@ -6,8 +6,30 @@ import re
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 os.environ['TESSDATA_PREFIX'] = 'C:\Program Files\Tesseract-OCR\\tessdata'
 
-image=cv2.imread('./src/test/AdharCard.jpg')
+image=cv2.imread('./src/test/adhar5.jpg')
+template = cv2.imread('./src/imgs/templates/aadharFront.jpg')
 
+bnwImg1=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+bnwImg2=cv2.cvtColor(template,cv2.COLOR_BGR2GRAY)
+cv2.imshow('GRAY Template IMG',template)
+cv2.waitKey(0)
+
+""" Match """
+
+img_copy = image.copy()
+result = cv2.matchTemplate(img_copy, template, cv2.TM_CCOEFF_NORMED)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+w, h = template.shape[1], template.shape[0]
+
+cropped = img_copy[max_loc[1]:max_loc[1]+h, max_loc[0]:max_loc[0]+w]
+cv2.imshow('GRAY Template IMG',cropped)
+cv2.waitKey(0)
+
+
+
+
+""" 
 image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Driving",image)
 cv2.waitKey(0)
@@ -26,4 +48,4 @@ print(str(text).replace('\t','').replace('\n\n','\n'))
 #     with open(filePath, "w", encoding="utf-8") as file:
 #             file.write(str(text).replace('\t','').replace('\n\n','\n'))
 # else:
-#     print("didn't found")
+#     print("didn't found") """
