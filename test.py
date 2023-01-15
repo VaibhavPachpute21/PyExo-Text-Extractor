f0612042550555884c5bae019f4cea8669265fc4
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-01-15 14:52:02 +0530

    started with driving data

--- test.py
+++ test.py
@@ -2,20 +2,25 @@ import pytesseract
 import cv2
 import os
 import re
-import numpy as np
+from datetime import datetime
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 os.environ['TESSDATA_PREFIX'] = 'C:\Program Files\Tesseract-OCR\\tessdata'
-image=cv2.imread('D:\Web\eCell\src\\test\pan1.jpg')
 
-text = pytesseract.image_to_string(image, lang='eng+hin+mar')
+path = "D:\Web\eCell\src\\test"
+allFiles = os.listdir(path)
+print(allFiles)
+for file in allFiles:
+    imPath = path+'\\'+file
+    # print(imPath)
+    image = cv2.imread(imPath)
+    text = pytesseract.image_to_string(image, lang='eng+hin+mar')
+    # print(str(text))
 
-
-
-if str(text).__contains__('Permanent Account Number'):
-    PanNO=str(text).split('Number')[1].split('\n')[1]
-    filePath = os.getcwd()+'\\src\\extracts\\%s_pan.txt'%PanNO
-    with open(filePath, "w", encoding="utf-8") as file:
-            file.write(str(text).replace('\t','').replace('\n\n','\n'))
-else:
-    print("didn't found") 
+    if str(text).__contains__('DRIVE' or 'DRIVING LICENCE'):
+        driLicenceNo = re.search(r"\d{4}", str(text))
+        filePath = os.getcwd()+'\\src\\extracts\\%s_drving.txt'%file
+        with open(filePath, "w", encoding="utf-8") as file:
+            file.write(str(text).replace('\t', '').replace('\n\n', '\n'))
+    else:
+        print("didn't found: ", imPath)
