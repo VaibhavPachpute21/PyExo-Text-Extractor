afa9a4655cd147058c2446511d41b79dca88a96a
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-01-15 17:07:18 +0530

    Split

--- test.py
+++ test.py
@@ -8,18 +8,29 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tess
 os.environ['TESSDATA_PREFIX'] = 'C:\Program Files\Tesseract-OCR\\tessdata'
 
 path = os.getcwd()+"\\src\\test"
+
+voterList = ['voter.jpg','voter1.jpg','voter2.jpg','voter3.jpg','voter4.jpg']
 allFiles = os.listdir(path)
-print(allFiles)
-for file in allFiles:
+
+for file in voterList:
     imPath = path+'\\'+file
     # print(imPath)
+    filePath = os.getcwd()+'\\src\\extracts\\%s_voter.txt'%file
     image = cv2.imread(imPath)
     text = pytesseract.image_to_string(image, lang='eng+hin+mar')
-    print(str(text))
+    
 
-    if str(text).__contains__('DRIVE' or 'DRIVING LICENCE'):
-        driLicenceNo = re.search(r"\d{4}", str(text))
-        filePath = os.getcwd()+'\\src\\extracts\\%s_drving.txt'%file
+    if str(text).__contains__('ELECTION'):
+        if str(text).__contains__('Elector') or str(text).__contains__("Eléctor's") or str(text).__contains__("Elector's") or str(text).__contains__("Elector's Name"):
+            print("HI")
+            print()
+            """ full_name = str(text).split('Elector')[1] """
+            """ print(full_name) """
+            print(str(text).split('Elector'))
+            print('\n')
+            print('\n')
+
+
         with open(filePath, "w", encoding="utf-8") as file:
             file.write(str(text).replace('\t', '').replace('\n\n', '\n'))
     else:
