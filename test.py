10b079abd913ed1d44dbe7ee5cf0c32e1f0bdc8f
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-01-15 22:08:07 +0530

    sharpen

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import cv2
 import os
 import re
 from datetime import datetime
+import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 os.environ['TESSDATA_PREFIX'] = 'C:\Program Files\Tesseract-OCR\\tessdata'
@@ -18,40 +19,47 @@ for file in voterList:
     # print(imPath)
     filePath = os.getcwd()+'\\src\\extracts\\%s_voter.txt'%file
     image = cv2.imread(imPath)
-    text = pytesseract.image_to_string(image, lang='eng+hin+mar')
+    kernel=np.array([[-1,-1,-1], [-1,5,-1], [-1,-1,-1]])
+    sharpened = cv2.filter2D(image, -1, kernel)
+    cv2.imshow("sharp",sharpened)
+    cv2.waitKey(0)
+    text = pytesseract.image_to_string(sharpened, lang='eng+hin+mar')
+
+    with open(filePath, "w", encoding="utf-8") as file:
+                file.write(str(text).replace('\t', '').replace('\n\n', '\n'))
     
 
-    if str(text).__contains__('ELECTION'):
-        electors_Name=''
-        father_name=''
-        husband_name=''
-        gender=''
+    # if str(text).__contains__('ELECTION'):
+    #     electors_Name=''
+    #     father_name=''
+    #     husband_name=''
+    #     gender=''
 
-        newArr=str(text).split('\n')
-        for i in newArr:
-            if ("Elector") in i :
-                electors_Name= i.split('Name')[1].replace(":","").strip()
-            if ("Eléctor") in i:
-                electors_Name= i.split('Name')[1].replace(":","").strip()
-            if ("Father") in i:
-                father_name= i.split('Name')[1].replace(":","").strip()
-            if ("Husband") in i:
-                husband_name=i.split('Name')[1].replace(":","").strip()
-            if "Male" in i:
-                gender="Male"
-            if "Female" in i:
-                gender="Female"
+    #     newArr=str(text).split('\n')
+    #     for i in newArr:
+    #         if ("Elector") in i :
+    #             electors_Name= i.split('Name')[1].replace(":","").strip()
+    #         if ("Eléctor") in i:
+    #             electors_Name= i.split('Name')[1].replace(":","").strip()
+    #         if ("Father") in i:
+    #             father_name= i.split('Name')[1].replace(":","").strip()
+    #         if ("Husband") in i:
+    #             husband_name=i.split('Name')[1].replace(":","").strip()
+    #         if "Male" in i:
+    #             gender="Male"
+    #         if "Female" in i:
+    #             gender="Female"
 
-        voterObj={
-            "Name:": electors_Name,
-            "Father Name:":father_name,
-            "Husband Name:":husband_name,
-            "Gender:":gender
-        }
-        voterArr.append(voterObj)
+    #     voterObj={
+    #         "Name:": electors_Name,
+    #         "Father Name:":father_name,
+    #         "Husband Name:":husband_name,
+    #         "Gender:":gender
+    #     }
+    #     voterArr.append(voterObj)
             
-    else:
-        # print(str(text))
-        # print("didn't found: ", imPath)
-        pass
-print(voterArr)
+    # else:
+    #     # print(str(text))
+    #     # print("didn't found: ", imPath)
+    #     pass
+# print(voterArr)
