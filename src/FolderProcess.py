8bb784b92032de43cc970f70b13e69baeb8d2c73
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-01-18 14:27:24 +0530

    First run success

--- src/FolderProcess.py
+++ src/FolderProcess.py
@@ -39,7 +39,7 @@ class ExtractAndForward():
     def ExtractAadhar(self):
         filesPaths = self.FindPaths()
         for x in filesPaths:
-            print(x)
+            
             ExtractData(x)
 
     def FindPaths(self):
