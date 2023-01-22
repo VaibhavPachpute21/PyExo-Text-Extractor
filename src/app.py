26a6720be1ba977349193f649bb7bb6323051cf3
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-01-22 22:16:46 +0530

    cloudinary download

--- src/app.py
+++ src/app.py
@@ -1,23 +1,37 @@
 from flask import Flask
-from flask import Flask, render_template, request, session
-import os
+from flask import Flask, render_template, request
 from PyExo import PyExo
-import json
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import requests
 
-
+cloudinary.config(
+  cloud_name = "dvvzlzude",
+  api_key = "819426927953279",
+  api_secret = "JAPvS4ZO-hlUtSipa6-BzpRfg8M"
+)
 
 app = Flask(__name__, template_folder='templates')
-
  
 @app.route('/')
 def index():
     return render_template('index.html')
- 
-@app.route('/ProcessDoc/<string:url>', methods=['POST'])
-def ProcessDoc(url):
-    url=json.loads(url)
+
+@app.route('/ProcessDoc', methods=['POST'])
+def ProcessDoc():
+    obj = request.get_json()
+    print(obj)
+    url = obj['URL']
+    public_id=obj['public_id']
     print("URL:",url)
-    return('/')
+    # Download the PDF
+    pdf_url = cloudinary.utils.cloudinary_url(public_id, format="pdf")[0]
+    response = requests.get(pdf_url)
+    # Save the PDF to a local file
+    with open("downloaded_pdf.pdf", "wb") as f:
+        f.write(response.content)
+    return request.data
 
  
 if __name__=='__main__':
