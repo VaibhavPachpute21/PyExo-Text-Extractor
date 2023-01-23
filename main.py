693934ac45ba40724241a9a3307be5d957dcd991
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-01-23 18:32:23 +0530

    rest

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ os.environ['TESSDATA_PREFIX'] = 'C:\Program Files\Tesseract-OCR\\tessdata'
 
 # Extract from pdf
 # Pls provide full path to PDF
-# data = PyExo.Extract_From_Pdf(pdfFile=r'D:\Web\\eCell\src\\test\\test.pdf')
+# data = PyExo.Extract_From_Pdf(pdfFile=r'D:\Web\\iHack\src\\test\\test.pdf')
 
 # Extract from Word
 # Pls provide full path to docx file
-# data = PyExo.Extract_From_Doc(wordFile=r'D:\Web\\eCell\src\\test\\test.docx')
+# data = PyExo.Extract_From_Doc(wordFile=r'D:\Web\\iHack\src\\test\\test.docx')
 
 # Extract from Images
 # Provide path to folder where images are stored
-data = PyExo.Extract_From_Images(folderPath=r'D:\Web\\eCell\src\\test') 
+data = PyExo.Extract_From_Images(folderPath=r'D:\Web\\iHack\src\\test') 
 # Printing received data
 print("Output",data)
